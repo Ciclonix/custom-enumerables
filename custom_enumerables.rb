83ff6751320282080694fdef259e70d86b889a9d
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-09-04 09:26:28 +0200

    Add methdos my_count, my_map and my_inject

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -34,6 +34,35 @@ module Enumerable
     end
     return true
   end
+
+  def my_count
+    return self.size unless block_given?
+
+    count = 0
+    self.each do |x|
+      count += 1 if yield x
+    end
+    return count
+  end
+
+  def my_map
+    temp = []
+    self.each do |x|
+      temp << yield(x)
+    end
+    return temp
+  end
+
+  def my_inject(result = nil)
+    self.my_each_with_index do |x, idx|
+      if result == nil && idx == 0
+        result = self[0]
+      else
+        result = yield result, x
+      end
+    end
+    return result
+  end
 end
 
 
