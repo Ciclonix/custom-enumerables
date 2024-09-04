62f5984f0b2ef0769ca5fcbcafd84db652f930bd
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-09-04 09:36:06 +0200

    Clean code

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -1,6 +1,6 @@
 module Enumerable
   def my_each_with_index
-    for idx in (0...self.size)
+    for idx in (0...size)
       yield self[idx], idx
     end
     return self
@@ -8,38 +8,38 @@ module Enumerable
 
   def my_select
     temp = []
-    self.each do |x|
+    each do |x|
       temp << x if yield x
     end
     return temp
   end
 
   def my_all?
-    self.each do |x|
+    each do |x|
       return false unless yield x
     end
     return true
   end
 
   def my_any?
-    self.each do |x|
+    each do |x|
       return true if yield x
     end
     return false
   end
 
   def my_none?
-    self.each do |x|
+    each do |x|
       return false if yield x
     end
     return true
   end
 
   def my_count
-    return self.size unless block_given?
+    return size unless block_given?
 
     count = 0
-    self.each do |x|
+    each do |x|
       count += 1 if yield x
     end
     return count
@@ -47,19 +47,19 @@ module Enumerable
 
   def my_map
     temp = []
-    self.each do |x|
+    each do |x|
       temp << yield(x)
     end
     return temp
   end
 
   def my_inject(result = nil)
-    self.my_each_with_index do |x, idx|
-      if result == nil && idx == 0
-        result = self[0]
-      else
-        result = yield result, x
-      end
+    my_each_with_index do |x, idx|
+      result = if result.nil? && idx.zero?
+                 self[0]
+               else
+                 yield result, x
+               end
     end
     return result
   end
